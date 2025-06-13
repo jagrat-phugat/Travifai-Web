9f285b788d1fcd5eda72e02a6d730f82e6211756
Mihir <2022chb1052>
2025-06-13 15:48:30 +0530

    New Change

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import Hero from "../components/Hero/Hero.jsx";
+// import Hero from "../components/Hero/Hero.jsx";
 import MacbookProComponent from "../components/MacbookProComponent/MacbookProComponent.jsx";
 import Innovation from "../components/Inovation/Inovation.jsx";
 import Controller from "../components/Controller/Controller.jsx";
@@ -7,12 +7,12 @@ import Description from "../components/Description/Description.jsx";
 import Faq from "../components/Faq/Faq.jsx";
 import WhyUs from "../components/Whyus/Whyus.jsx";
 import Blank from "../components/Blank/Blank.jsx";
-import Contact from "../components/Contact/Contact.jsx";
-import OurMission from "../components/Ourmission/Ourmission.jsx";
-import OurValues from "../components/Our Values/OurValues.jsx";
-import WhoAreWe from "../components/WhoAreWe/WhoAreWe.jsx";
-import Features from "../components/Features/Features.jsx";
-import Newsletter from "../components/Newsletter/Newsletter.jsx";
+// import Contact from "../components/Contact/Contact.jsx";
+// import OurMission from "../components/Ourmission/Ourmission.jsx";
+// import OurValues from "../components/Our Values/OurValues.jsx";
+// import WhoAreWe from "../components/WhoAreWe/WhoAreWe.jsx";
+// import Features from "../components/Features/Features.jsx";
+// import Newsletter from "../components/Newsletter/Newsletter.jsx";
 
 const LandingPage = () => {
   return (
